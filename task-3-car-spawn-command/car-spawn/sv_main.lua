fcf931f821064c7a0d2cf57149ac936778bc7ca0
Adam <adamgranville1@gmail.com>
2022-08-06 13:07:34 +0100

    task 3 solution

---
+++ task-3-car-spawn-command/car-spawn/sv_main.lua
@@ -0,0 +1,7 @@
+RegisterCommand('vehicle', function(source,args)
+    local src = source
+    local ped = GetPlayerPed(src)
+    local coords = GetEntityCoords(ped)
+    local carName = args[1]
+    CreateVehicle(carName, coords.x + 2, coords.y, coords.z)
+end)
