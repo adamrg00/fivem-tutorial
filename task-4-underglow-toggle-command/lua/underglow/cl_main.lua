2498922f5c3feb0112b527ce508ec6ef918d26ee
Adam <adamgranville1@gmail.com>
2022-08-07 17:47:19 +0100

    Small tweaks to solution

--- task-4-underglow-toggle-command/lua/underglow/cl_main.lua
+++ task-4-underglow-toggle-command/lua/underglow/cl_main.lua
@@ -1,14 +1,12 @@
 RegisterNetEvent('vehicle-underglow-on', function(vehicleNetId)
-    local src = source
     local vehicle = NetToVeh(vehicleNetId)
     for i = 0,3 do
         SetVehicleNeonLightEnabled(vehicle, i, true)
     end
-    SetVehicleNeonLightsColour(vehicle, 255, 150, 0)
+    SetVehicleNeonLightsColour(vehicle, 2, 21, 255)
 end)
 
 RegisterNetEvent('vehicle-underglow-off', function(vehicleNetId)
-    local src = source
     local vehicle = NetToVeh(vehicleNetId)
     for i = 0,3 do
         SetVehicleNeonLightEnabled(vehicle, i, false)
--- task-4-underglow-toggle-command/lua/underglow/sv_main.lua
+++ task-4-underglow-toggle-command/lua/underglow/sv_main.lua
@@ -1,12 +1,13 @@
 RegisterCommand('underglow', function(source, args)
-    local ped = GetPlayerPed(source)
+    local src = source
+    local ped = GetPlayerPed(src)
     local vehicle = GetVehiclePedIsIn(ped)
-    if vehicle then -- This line checks that the ped is in a vehicle
-        local vehicleNetId = NetworkGetNetworkIdFromEntity(vehicle)
+    if vehicle ~= 0 then -- This line checks that the ped is in a vehicle
+        local vehNet = NetworkGetNetworkIdFromEntity(vehicle)
         if args[1] == 'on' then
-            TriggerClientEvent('vehicle-underglow-on', source, vehicleNetId)
+            TriggerClientEvent('vehicle-underglow-on', src, vehNet)
         elseif args[1] == 'off' then
-            TriggerClientEvent('vehicle-underglow-off', source, vehicleNetId)
+            TriggerClientEvent('vehicle-underglow-off', src, vehNet)
         else
             print('no argument specified to underglow command')
         end
